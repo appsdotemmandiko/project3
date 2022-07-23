fb119bf7238fa4f455cef3817e52ea83faf2563b
emmandiko <apps.emmandiko@gmail.com>
2022-07-23 11:10:21 +0300

    CD

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=emz
+export POSTGRES_PASSWORD=Too2simple.
+export POSTGRES_HOST=database-instance-emz.ctqafta6wf00.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=rn:aws:s3:::myemzbucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
