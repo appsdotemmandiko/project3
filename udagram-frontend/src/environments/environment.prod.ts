00e97748b23ff5ac97dabb72b126fe212c63af82
Emma Ndiko <98321554+appsdotemmandiko@users.noreply.github.com>
2022-07-23 20:56:04 +0300

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6c28a30907264cd8acf5600242ac0c9-2048100409.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
